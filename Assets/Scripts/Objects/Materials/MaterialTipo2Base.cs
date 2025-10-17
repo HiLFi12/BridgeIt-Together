ddeb821d225bbabd0ba6d57bc37cd7fac1f88908
Frank-Sip <francoberlin1010@gmail.com>
2025-10-17 20:56:53 -0300

    Add UI assets and improve material UI management
    Added new prehistoric material UI sprites and meta files. Updated prefab and scene files to support multiple material UI layers and indices. Enhanced MaterialTipo2Base to implement IUIActivatable and expose UI index. Refactored PlayerUIManager to support named UI groups and improved bridge quadrant registration and activation logging.

--- Assets/Scripts/Objects/Materials/MaterialTipo2Base.cs
+++ Assets/Scripts/Objects/Materials/MaterialTipo2Base.cs
@@ -1,10 +1,15 @@
+using BridgeItTogether.Gameplay.Abstractions;
 using UnityEngine;
 
 /// <summary>
 /// Script padre para Material Tipo 2: maneja construcción y estado PuedeConstruirse.
 /// </summary>
-public class MaterialTipo2Base : MaterialBaseInteractable
+public class MaterialTipo2Base : MaterialBaseInteractable, IUIActivatable
 {
+    [Header("UI Configuration")]
+    [SerializeField] private int uiIndex = 0;
+    public int UIIndex => uiIndex;
+
     protected override int LayerIndex => 1; 
 
     protected override void Awake()
@@ -16,4 +21,8 @@ public class MaterialTipo2Base : MaterialBaseInteractable
         }
     }
 
+    public void SetUIIndex(int index)
+    {
+        uiIndex = index;
+    }
 }
