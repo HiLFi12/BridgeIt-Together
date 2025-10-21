afc27cb6c3ce35f922004c8ed9ba897c0c61419d
Frank-Sip <francoberlin1010@gmail.com>
2025-10-21 16:09:24 -0300

    Now both players can be Played with controllers

--- Assets/Scripts/InputActions/InputDeviceManager.cs
+++ Assets/Scripts/InputActions/InputDeviceManager.cs
@@ -1,55 +1,148 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Users;
+using System.Collections.Generic;
 
 public class InputDeviceManager : MonoBehaviour
 {
     [SerializeField] private PlayerInput player1;
     [SerializeField] private PlayerInput player2;
 
-    private InputUser user1;
-    private InputUser user2;
-    private bool player2Assigned = false; // Para saber si ya tiene gamepad
+    private Gamepad player1Gamepad = null;
+    private Gamepad player2Gamepad = null;
+
+    private HashSet<InputDevice> player1Devices = new HashSet<InputDevice>();
+    private HashSet<InputDevice> player2Devices = new HashSet<InputDevice>();
 
     void Awake()
     {
-        // Crear usuarios independientes
-        user1 = InputUser.CreateUserWithoutPairedDevices();
-        user2 = InputUser.CreateUserWithoutPairedDevices();
+        // Crear instancias separadas de ActionAssets
+        player1.actions = Instantiate(player1.actions);
+        player2.actions = Instantiate(player2.actions);
 
-        // Asignar teclado al Player1
+        // Deshabilitar el auto-switching
+        player1.neverAutoSwitchControlSchemes = true;
+        player2.neverAutoSwitchControlSchemes = true;
+
+        // Asignar dispositivos iniciales
         if (Keyboard.current != null)
-            InputUser.PerformPairingWithDevice(Keyboard.current, user1);
-
-        // Asignar primer gamepad al Player2 si está conectado al inicio
-        if (Gamepad.all.Count > 0)
         {
-            InputUser.PerformPairingWithDevice(Gamepad.all[0], user2);
-            player2Assigned = true;
+            player1Devices.Add(Keyboard.current);
         }
 
-        // Asociar actions
-        user1.AssociateActionsWithUser(player1.actions);
-        user2.AssociateActionsWithUser(player2.actions);
+        if (Gamepad.all.Count > 0)
+        {
+            player2Gamepad = Gamepad.all[0];
+            player2Devices.Add(player2Gamepad);
+        }
+        if (Gamepad.all.Count > 1)
+        {
+            player1Gamepad = Gamepad.all[1];
+            player1Devices.Add(player1Gamepad);
+        }
+
+        // Configurar filtros de dispositivos
+        SetupDeviceFilters();
+
+        // Habilitar acciones
+        player1.actions.Enable();
+        player2.actions.Enable();
 
         // Suscribirse a cambios de dispositivos
         InputSystem.onDeviceChange += OnDeviceChange;
     }
 
+    private void SetupDeviceFilters()
+    {
+        // Convertir HashSet a lista para Player1
+        var p1DeviceList = new List<InputDevice>(player1Devices);
+        if (p1DeviceList.Count > 0)
+        {
+            player1.actions.devices = new UnityEngine.InputSystem.Utilities.ReadOnlyArray<InputDevice>(p1DeviceList.ToArray());
+        }
+
+        // Convertir HashSet a lista para Player2
+        var p2DeviceList = new List<InputDevice>(player2Devices);
+        if (p2DeviceList.Count > 0)
+        {
+            player2.actions.devices = new UnityEngine.InputSystem.Utilities.ReadOnlyArray<InputDevice>(p2DeviceList.ToArray());
+        }
+
+        Debug.Log($"Player1 devices: {player1Devices.Count}");
+        Debug.Log($"Player2 devices: {player2Devices.Count}");
+    }
+
     private void OnDeviceChange(InputDevice device, InputDeviceChange change)
     {
-        // Cuando se conecta un nuevo gamepad
-        if (change == InputDeviceChange.Added && device is Gamepad)
+        if (!(device is Gamepad newGamepad))
+            return;
+
+        switch (change)
         {
-            if (!player2Assigned)
-            {
-                InputUser.PerformPairingWithDevice(device, user2);
-                player2Assigned = true;
-                Debug.Log($"Gamepad {device.displayName} asignado a Player2");
-            }
+            case InputDeviceChange.Added:
+            case InputDeviceChange.Reconnected:
+                // Si es el gamepad de Player2
+                if (player2Gamepad != null && AreGamepadsSame(newGamepad, player2Gamepad))
+                {
+                    player2Gamepad = newGamepad;
+                    player2Devices.Remove(device); // Remover versión vieja
+                    player2Devices.Add(newGamepad);
+                    SetupDeviceFilters();
+                    Debug.Log($"Gamepad reconectado a Player2");
+                    return;
+                }
+
+                // Si es el gamepad de Player1
+                if (player1Gamepad != null && AreGamepadsSame(newGamepad, player1Gamepad))
+                {
+                    player1Gamepad = newGamepad;
+                    player1Devices.Remove(device); // Remover versión vieja
+                    player1Devices.Add(newGamepad);
+                    SetupDeviceFilters();
+                    Debug.Log($"Gamepad reconectado a Player1");
+                    return;
+                }
+
+                // Nuevo gamepad
+                if (player2Gamepad == null)
+                {
+                    player2Gamepad = newGamepad;
+                    player2Devices.Add(newGamepad);
+                    SetupDeviceFilters();
+                    Debug.Log($"Gamepad asignado a Player2");
+                }
+                else if (player1Gamepad == null)
+                {
+                    player1Gamepad = newGamepad;
+                    player1Devices.Add(newGamepad);
+                    SetupDeviceFilters();
+                    Debug.Log($"Gamepad asignado a Player1");
+                }
+                break;
+
+            case InputDeviceChange.Removed:
+            case InputDeviceChange.Disconnected:
+                // Remover de los sets pero mantener la referencia para reconocer al reconectar
+                if (player2Devices.Contains(device))
+                {
+                    player2Devices.Remove(device);
+                    SetupDeviceFilters();
+                    Debug.Log($"Gamepad removido de Player2");
+                }
+                else if (player1Devices.Contains(device))
+                {
+                    player1Devices.Remove(device);
+                    SetupDeviceFilters();
+                    Debug.Log($"Gamepad removido de Player1");
+                }
+                break;
         }
+    }
+
+    private bool AreGamepadsSame(Gamepad a, Gamepad b)
+    {
+        return a.description.manufacturer == b.description.manufacturer &&
+               a.description.product == b.description.product &&
+               a.description.serial == b.description.serial;
     }
 
     private void OnDestroy()
