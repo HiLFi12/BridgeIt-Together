a6c9ea2f14159233b579e8de4fc37c70f2c6fa60
Frank-Sip <francoberlin1010@gmail.com>
2025-12-02 21:50:00 -0300

    Added pop up diable logic to BVictory, Pause and Defeat UI

---
+++ Assets/Scripts/UI/ToggleObjectOnEnable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ToggleObjectOnEnable : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToDisable;
+
+    private void OnEnable()
+    {
+        if (objectToDisable != null)
+        {
+            objectToDisable.SetActive(false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (objectToDisable != null)
+        {
+            objectToDisable.SetActive(true);
+        }
+    }
+}
+
